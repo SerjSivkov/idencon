8c99aad13dcfad806a3fb1924a6c4ac07cba586a
Sergey Sivkov <s.sivkov@avia-centr.ru>
2020-03-19 21:10:04 +0300

    add identicon class

---
+++ lib/identicon_gem.rb
@@ -0,0 +1,13 @@
+module IdenticonGem
+  class Identicon
+    def initialize(user_name, path = nil); end
+
+    def generate
+      puts "YOU ARE AWESOME!!"
+    end
+
+    private
+
+    def save_image!; end
+  end
+end
